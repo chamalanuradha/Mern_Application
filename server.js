7098cd578ae9dd8d3db4298d6f06cdb67dc31f76
chamalanuradha <chamalanuradha98765@gmail.com>
2024-08-06 09:20:57 +0530

    configure with  MongoDB atlas

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 
-const port = 8080;
+const PORT = 8080;
+const DB_URL = 'mongodb+srv://anuradhachamal14:chamal98@mernapp.jlc51bz.mongodb.net/?retryWrites=true&w=majority&appName=MernApp';
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+
+mongoose.connect(DB_URL)
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.log('Database connection error', err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
    
 })
