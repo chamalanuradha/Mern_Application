1e4312395222327984ebfba8da47da43fb45b755
chamalanuradha <chamalanuradha98765@gmail.com>
2024-08-09 12:24:44 +0530

    Update navbar and Addd new components

--- client/src/component/navbar.js
+++ client/src/component/navbar.js
@@ -1,28 +1,37 @@
 import React from 'react'
+import './navbar.css'
 
 export default function navbar() {
   return (
-    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-    <div class="container-fluid">
-      <a class="navbar-brand " href="/">Daily Mirror</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Posts</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-        </ul>
-      </div>
+    <nav className ="navbar navbar-expand-lg">
+  <div className ="container-fluid">
+    <a className ="navbar-brand" href="/">Daily Master</a>
+    <button className ="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className ="navbar-toggler-icon"></span>
+    </button>
+    <div className ="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="#">Login</a>
+        </li>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
   )
 }
 
